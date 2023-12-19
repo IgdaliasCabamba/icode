f3c82391071fdf10c3e5223862f60abcf8ca19bd
Igdálias Cabamba <igdaliasmussulveiracabamba@gmail.com>
2023-12-20 00:44:51 +0100

    added untracked files

---
+++ .concepts/ipython_console.py
@@ -0,0 +1,59 @@
+from qtpy.QtWidgets import QApplication
+from qtconsole.rich_jupyter_widget import RichJupyterWidget
+from qtconsole.inprocess import QtInProcessKernelManager
+
+
+class ConsoleWidget(RichJupyterWidget):
+
+    def __init__(self, customBanner=None, *args, **kwargs):
+        super(ConsoleWidget, self).__init__(*args, **kwargs)
+
+        if customBanner is not None:
+            self.banner = customBanner
+
+        self.font_size = 10
+        self.kernel_manager = QtInProcessKernelManager()
+        self.kernel_manager.start_kernel(show_banner=False)
+        self.kernel_manager.kernel.gui = 'qt'
+        self.kernel_client = self._kernel_manager.client()
+        self.kernel_client.start_channels()
+
+        def stop():
+            self.kernel_client.stop_channels()
+            self.kernel_manager.shutdown_kernel()
+
+        self.exit_requested.connect(stop)
+
+    def push_vars(self, variableDict):
+        """
+        Given a dictionary containing name / value pairs, push those variables
+        to the Jupyter console widget
+        """
+        self.kernel_manager.kernel.shell.push(variableDict)
+
+    def clear(self):
+        """
+        Clears the terminal
+        """
+        self._control.clear()
+
+    def print_text(self, text):
+        """
+        Prints some plain text to the console
+        """
+        self._append_plain_text(text)
+
+    def execute_command(self, command):
+        """
+        Execute a command in the frame of the console widget
+        """
+        self._execute(command, False)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    widget = ConsoleWidget()
+    widget1 = ConsoleWidget()
+    widget.show()
+    widget1.show()
+    app.exec_()
